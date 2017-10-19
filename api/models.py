5998fa97d1e71428a4851e979e0110de4e694a81
Erastov Fedor <fedor_erastov@mail.ru>
2017-10-19 19:34:10 +0300

    Add new attributes

--- api/models.py
+++ api/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
+    name = models.CharField(null=True, max_length=100)
 
     def __str__(self):
         return str()
 
 
 class Subcategory(models.Model):
+    name = models.CharField(null=True, max_length=100)
     category = models.ForiegnKey(Category)
 
     def __str__(self):
@@ -15,6 +17,7 @@ class Subcategory(models.Model):
 
 
 class Dish(models.Model):
+    name = models.CharField(null=True, max_length=100)
     subcategory = models.ForiegnKey(Subcategory)
     
     def __str__(self):
@@ -22,12 +25,14 @@ class Dish(models.Model):
 
 
 class Operator(models.Model):
+    name = models.CharField(null=True, max_length=100)
     
     def __str__(self):
         return str()
 
 
 class Restaurant(models.Model):
+    name = models.CharField(null=True, max_length=100)
     
     def __str__(self):
         return str()
